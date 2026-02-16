3a759b3cfef291b791918b2c0e85e319d32a15da
ahsanyousaf786 <ahsanyousaf490@gmail.com>
2026-02-16 18:44:56 +0500

    created hero section of the website

---
+++ my-portfolio/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className="hero-img-section">
+                <img src="src/assets/hero.png" alt="hero-img" />
+            </div>
+            <div className="hero-text">
+                <h1>HI, I Am Ahsan</h1>
+            <p>Software Engineer</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
