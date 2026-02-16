e3b1ee8c169d6bea27381ca405dbfe959e4b41ef
ahsanyousaf786 <ahsanyousaf490@gmail.com>
2026-02-16 18:45:28 +0500

    crested service section componeent after hero

---
+++ my-portfolio/src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className='services'>
+            <h2>My Services</h2>
+            <div className="services-container">
+                <div id="service1" className="service-card">Service 1</div>
+                <div id="service2" className="service-card">Service 2</div>
+                <div id="service3" className="service-card">Service 3</div>
+                <div id="service4" className="service-card">Service 4</div>
+            </div>
+        </div>
+    )
+}
+
+export default Services
