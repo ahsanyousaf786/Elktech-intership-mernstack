6a09807418aa61207be4d88c95ffaadc695aefd0
ahsanyousaf786 <ahsanyousaf490@gmail.com>
2026-02-17 19:05:52 +0500

    updated service.jsx and index.jsx

--- my-portfolio/src/components/Services.jsx
+++ my-portfolio/src/components/Services.jsx
@@ -5,10 +5,23 @@ function Services() {
         <div className='services'>
             <h2>My Services</h2>
             <div className="services-container">
-                <div id="service1" className="service-card">Service 1</div>
-                <div id="service2" className="service-card">Service 2</div>
-                <div id="service3" className="service-card">Service 3</div>
-                <div id="service4" className="service-card">Service 4</div>
+                <div id="service1" className="service-card">
+                    <div className="img">  image here  </div>
+                    <p><a href="">Read More</a></p>
+                </div>
+                <div id="service1" className="service-card">
+                    <div className="img">  image here  </div>
+                    <p><a href="">Read More</a></p>
+                </div>
+                <div id="service1" className="service-card">
+                    <div className="img">  image here  </div>
+                    <p><a href="">Read More</a></p>
+                </div>
+                <div id="service1" className="service-card">
+                    <div className="img">  image here  </div>
+                    <p><a href="">Read More</a></p>
+                </div>
+             
             </div>
         </div>
     )
