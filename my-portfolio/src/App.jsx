4063e40e6342fe79ecfaa2fc4b6d71c1adf24d8e
ahsanyousaf786 <ahsanyousaf490@gmail.com>
2026-02-18 17:20:26 +0500

    updated app.jsx, main.jsx, index.css

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import Navbar from './components/Navbar.jsx';
+// import Home from './pages/Home.jsx';
+import { Routes, Route } from 'react-router-dom'; 
+import './index.css';
+import About from './pages/About.jsx';
+import Home from "./pages/Home.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
+import Contact from "./pages/Contact.jsx";
+import Projects from "./components/Projects.jsx";
+
+import "./index.css";
 
 function App() {
- 
   return (
     <>
-    <h1>my first portfolio</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter} from 'react-router-dom'
 import './index.css'
 
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Services from './components/Services.jsx'
+import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <Hero />
-    <Services />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
