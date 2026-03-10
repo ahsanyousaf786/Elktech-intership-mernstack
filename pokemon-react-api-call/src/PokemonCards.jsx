7c7db42c72a14d4a26bbe4fb570618a87d56e864
ahsanyousaf786 <ahsanyousaf490@gmail.com>
2026-03-10 12:14:08 +0500

    destructured the pookemon data

--- pokemon-react-api-call/src/PokemonCards.jsx
+++ pokemon-react-api-call/src/PokemonCards.jsx
@@ -1,49 +1,66 @@
 
 
 export const PokemonCards = ({ pokemonData }) => {
+
+  const { name,
+    height,
+    weight,
+    sprites,
+    types,
+    stats,
+    abilities,
+    base_experience, } = pokemonData;
+
+  const image = sprites.other.dream_world.front_default;
+  const typeNames = types.map((curType) => curType.type.name).join(", ");
+  const speed = stats[5].base_stat;
+  const attack = stats[1].base_stat;
+  const ability = abilities
+    .map((abilityInfo) => abilityInfo.ability.name)
+    .slice(0, 1)
+    .join(", ");
+
+
   return (
     <li className="pokemon-card">
       <figure>
         <img
-          src={pokemonData.sprites.other.dream_world.front_default}
-          alt={pokemonData.name}
+          src={image}
+          alt={name}
           className="pokemon-image"
         />
       </figure>
-      <h1 className="pokemon-name">{pokemonData.name}</h1>
+      <h1 className="pokemon-name">{name}</h1>
       <div className="pokemon-info pokemon-highlight">
         <p>
-          {pokemonData.types.map((curType) => curType.type.name).join(", ")}
+          {typeNames}
         </p>
       </div>
 
       <div className="grid-three-cols">
         <p className="pokemon-info">
-          <span> Height:</span> {pokemonData.height}
+          <span> Height:</span> {height}
         </p>
         <p className="pokemon-info">
-          <span> Weight:</span> {pokemonData.weight}
+          <span> Weight:</span> {weight}
         </p>
         <p className="pokemon-info">
-          <span> speed:</span> {pokemonData.stats[5].base_stat}
+          <span> speed:</span> {speed}
         </p>
       </div>
 
       <div className="grid-three-cols">
         <div className="pokemon-info">
-          <p>{pokemonData.base_experience}</p>
+          <p>{base_experience}</p>
           <span> Experience:</span>
         </div>
         <div className="pokemon-info">
-          <p>{pokemonData.stats[1].base_stat}</p>
+          <p>{attack}</p>
           <span>Attack:</span>
         </div>
         <div className="pokemon-info">
           <p>
-            {pokemonData.abilities
-              .map((abilityInfo) => abilityInfo.ability.name)
-              .slice(0, 1)
-              .join(", ")}
+            {ability}
           </p>
           <span> Abilities: </span>
         </div>
